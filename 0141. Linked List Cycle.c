75ae2a380d368593d3257cc867158709bff22285
leeyu <leeyu0911@gmail.com>
2023-03-05 22:14:35 +0800

    Create 0141. Linked List Cycle.c

---
+++ 0141. Linked List Cycle.c
@@ -0,0 +1,28 @@
+//
+// Created by leeyu on 2023/3/5.
+//
+#include <stdio.h>
+#define bool int
+#define true 1
+#define false 0
+
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+
+bool hasCycle(struct ListNode *head) {
+    if (head == NULL)
+        return false;
+
+    struct ListNode *f = head;
+    struct ListNode *s = head;
+    while (f != NULL && f->next != NULL) {
+        f = f->next->next;
+        s = s->next;
+        if (f == s) return true;
+    }
+    return false;
+}
